72e6e20435f41a69a34edfe67548e3d497601261
Yura Zabolotin <zabolotiny@gmail.com>
2023-03-14 14:42:47 +0100

    Init

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '13.0'
+use_frameworks!
+
+target 'Texture+IGListKit' do
+  pod 'Texture/IGListKit+PinRemote', :git => 'https://github.com/Zabolotiny/Texture.git', :branch => 'releases/p11.9'
+end
---
+++ Texture+IGListKit/Texture_IGListKit.h
@@ -0,0 +1,18 @@
+//
+//  Texture_IGListKit.h
+//  Texture+IGListKit
+//
+//  Created by Yura Zabolotin on 14.03.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Texture_IGListKit.
+FOUNDATION_EXPORT double Texture_IGListKitVersionNumber;
+
+//! Project version string for Texture_IGListKit.
+FOUNDATION_EXPORT const unsigned char Texture_IGListKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Texture_IGListKit/PublicHeader.h>
+
+
